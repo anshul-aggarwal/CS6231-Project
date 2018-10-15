b25d3fb5126b6bbc2e67aa52679bf545a59df376
Anshul Aggarwal <anshulaggarwal@outlook.com>
2018-10-15 16:01:57 +0800

    Some parameter and output display changes

--- code/mlp_mlp_reduced.py
+++ code/mlp_mlp_reduced.py
@@ -7,20 +7,26 @@ from copy import deepcopy
 
 import random
 
+max_similarity = 0
+
 def isSimilar(vector1, vector2, min_similarity):
+    global max_similarity
     similar = 0
     for i in range(len(vector1)):
         if vector1[i] == vector2[i]:
             similar += 1
-    
-    if similar >= min_similarity:
+    if similar > max_similarity:
+            max_similarity = similar
+            print(similar)
+
+    if similar >= min_similarity:        
         return True
     else:
         return False
 
 #Read and Load the Datasets
-d1 = pd.read_csv("d1")
-d2 = pd.read_csv("d2")
+d1 = pd.read_csv("d1small")
+d2 = pd.read_csv("d2small")
 
 iterations = 100
 change_rate = 0.1
@@ -28,15 +34,15 @@ change_rate = 0.1
 
 #Separate into Attributes (50 only) and Target Variables
 y_d1 = d1.iloc[:,0]
-x_d1 = d1.iloc[:,1:50]
+x_d1 = d1.iloc[:,1:100]
 
 y_d2 = d2.iloc[:,0]
-x_d2 = d2.iloc[:,1:50]
+x_d2 = d2.iloc[:,1:100]
 
 
-#Converting target attribute into 10 classes from 100
-y_d1 = [y//10 for y in y_d1]
-y_d2 = [y//10 for y in y_d2]
+#Am converting target attribute into 10 classes from 100
+y_d1 = [(y-1)//10 for y in y_d1]
+y_d2 = [(y-1)//10 for y in y_d2]
 
 dimensions = len(x_d1.columns)
 #print(dimensions)
@@ -44,8 +50,8 @@ dimensions = len(x_d1.columns)
 change_count = int(change_rate*dimensions)
 
 #Define the ML Models (Both are Multi Layer Perceptrons)
-clf1 = MLPClassifier(solver='sgd',hidden_layer_sizes=(5, 5), random_state=1, alpha=0.01)
-clf2 = MLPClassifier(solver='sgd',hidden_layer_sizes=(5, 5), random_state=1, alpha=0.01)
+clf1 = MLPClassifier(solver='sgd',hidden_layer_sizes=(20, 10), random_state=1, alpha=0.001)
+clf2 = MLPClassifier(solver='sgd',hidden_layer_sizes=(30, 20), random_state=1, alpha=0.001)
 
 
 #Train the models
@@ -99,22 +105,22 @@ clf2_prob = clf2.predict_proba([test_vector]).flatten()
 
 print(test_vector)
 
-
 #Check for similarity
 similar_inputs_1 = []
 
 for i in range(len(x_d1)):
-    if isSimilar(test_vector, x_d1.iloc[i], int(0.7*dimensions)):
+    if isSimilar(test_vector, x_d1.iloc[i], int(0.5*dimensions)):
         similar_inputs_1.append(x_d1.iloc[i])
 
 
 similar_inputs_2 = []
 
 for i in range(len(x_d2)):
-    if isSimilar(test_vector, x_d2.iloc[i], int(0.7*dimensions)):
+    if isSimilar(test_vector, x_d2.iloc[i], int(0.5*dimensions)):
         similar_inputs_2.append(x_d2.iloc[i])
 
 #Points in training sets that are similar to the given vector
 si_1 = [tuple(x) for x in similar_inputs_1]
 si_2 = [tuple(x) for x in similar_inputs_2]
-print(set(si_1).intersection(set(si_2)))
+print("No. common")
+print(len(set(si_1).intersection(set(si_2))))
